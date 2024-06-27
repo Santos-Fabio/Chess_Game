371cf8c13ec797ad5e9aa44702ccdfedcfb26871
Santos-Fabio <santosfabio.ra@gmail.com>
2024-06-27 14:15:06 -0300

    Chess layer and printing the board. Print function differs a little bit form shown in the java course

--- src/application/App.java
+++ src/application/App.java
@@ -1,11 +1,11 @@
 package application;
-import boardgame.*;
+
+//import boardgame.*;
+import chess.ChessMatch;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Position pos = new Position(10, 15);
-        System.out.println(pos.toString());
-        Board board = new Board(8,8);
-        
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
