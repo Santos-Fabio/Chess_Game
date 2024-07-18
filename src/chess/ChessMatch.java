a7d5e19a74effa1b73ae6cffcf5e97dee3f78b77
Santos-Fabio <santosfabio.ra@gmail.com>
2024-07-18 14:41:19 -0300

    Queen (and Pawn bug fix)

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -221,6 +221,11 @@ public class ChessMatch {
         placeNewPiece('h',1,new Rook(board,Color.WHITE));
         placeNewPiece('a',8,new Rook(board,Color.BLACK));
         placeNewPiece('h',8,new Rook(board,Color.BLACK));
+
+        //Queens
+        placeNewPiece('d',1,new Queen(board,Color.WHITE));
+        placeNewPiece('d',8,new Queen(board,Color.BLACK));
+
         //Kings
         placeNewPiece('e',1,new King(board,Color.WHITE));
         placeNewPiece('e',8,new King(board,Color.BLACK));
